e973d6c65afa9ca5d10b6e33647e9bc7a274adb6
mansi <mansi.chaudhary@inmorphis.com>
2025-06-21 22:07:57 +0530

    Add files via upload

---
+++ 104_generate_array_between_two_integers_of_1_step_length.js
@@ -0,0 +1,12 @@
+function rangeBetween(start, end) {
+  let result = [];
+  
+  for (let i = start; i <= end; i++) {
+    result.push(i);
+  }
+  
+  return result;
+}
+
+console.log(rangeBetween(4, 7));   // [4, 5, 6, 7]
+console.log(rangeBetween(-4, 7));  // [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
