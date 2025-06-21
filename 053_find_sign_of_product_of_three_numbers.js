cd33aed1e3df861049a0cc12478fab20a003c291
mansi <mansi.chaudhary@inmorphis.com>
2025-06-21 21:25:48 +0530

    Add files via upload

---
+++ 053_find_sign_of_product_of_three_numbers.js
@@ -0,0 +1,10 @@
+let a = 3, b = -7, c = 2;
+let product = a * b * c;
+
+if (product > 0) {
+  alert("The sign is +");
+} else if (product < 0) {
+  alert("The sign is -");
+} else {
+  alert("The sign is 0");
+}
